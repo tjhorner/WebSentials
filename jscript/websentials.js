1149a1c3e4275595be9dcab7f8012733284d1171
GeekyGamer14 <tj.201@me.com>
2013-08-01 20:41:36 -0700

    pages

--- jscript/websentials.js
+++ jscript/websentials.js
@@ -1,5 +1,3 @@
 // WebSentials
 // By GeekyGamer14 (https://github.com/GeekyGamer14/WebSentials) - Open source web essentials.
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
-
-
