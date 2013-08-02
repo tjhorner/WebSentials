831acc4e046a7f7a415ddb9f1b82ba36a884229a
GeekyGamer14 <tj.201@me.com>
2013-08-01 21:43:04 -0700

    update to 1.0

--- jscript/websentials.js
+++ jscript/websentials.js
@@ -1,3 +1,5 @@
 // WebSentials
 // By GeekyGamer14 (https://github.com/GeekyGamer14/WebSentials) - Open source web essentials.
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
+
+
