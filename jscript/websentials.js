a43dfa0e77ac376491dd6287a09488acb127b472
GeekyGamer14 <tj.201@me.com>
2013-08-01 18:21:14 -0700

    updated buttons and js

--- jscript/websentials.js
+++ jscript/websentials.js
@@ -1,6 +1,18 @@
 // WebSentials
 // By GeekyGamer14 (https://github.com/GeekyGamer14/WebSentials) - Open source web essentials.
-// @require 
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
 
-//NoParse
-
+//button click
+function buttonCheck(element){
+	if($(element).attr('class') == 'btn-confirm'){
+		$(element).css('background-color', 'blue');
+	}else if($(element).attr('class') == 'btn-good'){
+		$(element).css('background-color', 'green');
+	}else{
+		$(element).css('background-color', '#969696');
+	}
+	$(element).mouseup($(element).css('color', undefined));
+}
+$('button').click(function(){
+	return this;
+});
